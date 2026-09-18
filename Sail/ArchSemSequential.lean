c816ea7ad03cce98d51acee252540fd4215fede8
Brian Campbell <Brian.Campbell@ed.ac.uk>
2026-09-18 11:03:39 +0100

    Clean up ArchSem sequential interpreter a little
    Some cosmetic improvements that I forgot to include in the last commit.

--- Sail/ArchSemSequential.lean
+++ Sail/ArchSemSequential.lean
@@ -2,39 +2,41 @@ import Sail.ArchSem
 
 import Std.Data.ExtHashMap
 
+-- A simple sequential interpretation of the ArchSem interface, roughly following the previous v1
+-- interface and Christopher Lang's archsem-lean model-specific version.
+
 namespace Sail.ArchSem
+
+open Sail.ArchSem
+variable [Arch]
 
 variable {Register : Type} {RegisterType : Register → Type} [DecidableEq Register] [Hashable Register]
 
-structure SequentialState (RegisterType : Register → Type) /- (c : ChoiceSource) -/ where
+-- No ChoiceSource here because the interface only does Fin n choices and we always pick 0
+
+structure SequentialState (RegisterType : Register → Type) where
   regs : Std.ExtDHashMap Register RegisterType
-  -- choiceState : c.α
   mem : Std.ExtHashMap Nat (BitVec 8)
   tags : Unit
   cycleCount : Nat -- Part of the concurrency interface. See `{get_}cycle_count`
   sailOutput : Array String -- TODO: be able to use the IO monad to run
   deriving Inhabited
 
-open Sail.ArchSem
-variable [Arch]
+abbrev ArchSemSeqM ue ty := EStateM (Sail.Error ue) (SequentialState Arch.register_type) ty
 
-abbrev M ue ty := EStateM (Sail.Error ue) (SequentialState Arch.register_type) ty
-
-variable {ue : Type}
-
-def writeByte (addr : Nat) (value : BitVec 8) : M ue PUnit := do
+def writeByte (addr : Nat) (value : BitVec 8) : ArchSemSeqM ue PUnit := do
   modify fun s => { s with mem := s.mem.insert addr value }
 
-def writeBytes (addr : Nat) (value : BitVec (8 * n)) : M ue PUnit := do
+def writeBytes (addr : Nat) (value : BitVec (8 * n)) : ArchSemSeqM ue PUnit := do
   let list := List.ofFn (λ i : Fin n => (addr + i.val, value.extractLsb' (8 * i.val) 8))
   List.forM list (λ (a, v) => writeByte a v)
 
-def readByte (addr : Nat) : M ue (BitVec 8) := do
+def readByte (addr : Nat) : ArchSemSeqM ue (BitVec 8) := do
   let .some s := (← get).mem.get? addr
     | throw (.OutOfMemoryRange addr)
   pure s
 
-def readBytes (size : Nat) (addr : Nat) : M ue (BitVec (8 * size)) :=
+def readBytes (size : Nat) (addr : Nat) : ArchSemSeqM ue (BitVec (8 * size)) :=
   match size with
   | 0 => pure default
   | 1 => do
@@ -47,7 +49,7 @@ def readBytes (size : Nat) (addr : Nat) : M ue (BitVec (8 * size)) :=
     have h : 8 * n + 8 = 8 * (n + 1) := by omega
     return h ▸ bytes.append b
 
-def interpretInstructionEffect : (eff : InstructionEffect) → M ue eff.ret
+def interpretInstructionEffect : (eff : InstructionEffect) → ArchSemSeqM ue eff.ret
   | .regRead reg _accessType => do
     let .some s := (← get).regs.get? reg
       | throw .Unreachable
@@ -76,11 +78,15 @@ def interpretInstructionEffect : (eff : InstructionEffect) → M ue eff.ret
   | .translationEnd _translationEnd
   | .returnException => pure ()
 
-def interpretSailEffects : PreSailM ue ty → M ue ty :=
+-- The main interpretation function
+
+def interpretSailEffects : PreSailM ue ty → ArchSemSeqM ue ty :=
   FreeM.liftM (fun
     | .inl (.error err) => throw err
     | .inl (.ok eff) => interpretInstructionEffect eff
     | .inr choice => if h : (0 : Nat) < choice then pure (⟨0, h⟩ : Fin choice) else throw .Unreachable)
+
+-- A function to provide a default toplevel for Sail models
 
 def main_of_sail_main (initialState : SequentialState Arch.register_type)
     (main : Unit → PreSailM ue Unit) : IO UInt32 := do
